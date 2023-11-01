3b4b7f6dc5288e9a449da915022d53ab7bbc0e99
张洪Heo <qewan@foxmail.com>
2023-11-01 14:29:43 +0800

    文档更新

---
+++ docs/.vitepress/dist/assets/install.md.ea6f3f5b.lean.js
@@ -0,0 +1,1 @@
+import{_ as t,o as d,c as e,Q as o}from"./chunks/framework.867ce416.js";const b=JSON.parse('{"title":"安装","description":"","frontmatter":{},"headers":[],"relativePath":"install.md","filePath":"install.md"}'),r={name:"install.md"},a=o("",14),h=[a];function n(s,l,i,p,c,m){return d(),e("div",null,h)}const f=t(r,[["render",n]]);export{b as __pageData,f as default};
